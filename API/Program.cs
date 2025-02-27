0ad8f0f925f6d5ac2b395025e2b239395f922557
Enrico Roselino <roselino.dev@gmail.com>
2025-02-27 15:13:20 +0700

    clean Program.cs

--- API/Program.cs
+++ API/Program.cs
@@ -1,12 +1,8 @@
 using BuildingBlocks.API;
-using Serilog;
-using Serilog.Events;
-using Shared.Configurations;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddOpenApi();
-
 builder.Services.AddLoggerSetup();
 
 var app = builder.Build();
