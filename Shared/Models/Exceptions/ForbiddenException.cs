d396d4661f85f1e6942609f0287836acc0c15a57
Enrico Roselino <roselino.dev@gmail.com>
2025-02-27 13:57:58 +0700

    add exception models

---
+++ Shared/Models/Exceptions/ForbiddenException.cs
@@ -0,0 +1,8 @@
+﻿namespace Shared.Models.Exceptions;
+
+public class ForbiddenException : Exception
+{
+    public ForbiddenException() : base("Access denied, you do not have permission to perform this action.")
+    {
+    }
+}
