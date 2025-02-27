78783f7becbd55636ad0174fcb8e268999f04ea0
Enrico Roselino <roselino.dev@gmail.com>
2025-02-27 17:06:50 +0700

    hotfix LoggingBehavior.cs

--- BuildingBlocks.API/Configurations/Mediator/LoggingBehavior.cs
+++ BuildingBlocks.API/Configurations/Mediator/LoggingBehavior.cs
@@ -4,7 +4,7 @@ using Microsoft.Extensions.Logging;
 namespace BuildingBlocks.API.Configurations.Mediator;
 
 internal sealed class LoggingBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
-    where TRequest : notnull, IRequest<TResponse>
+    where TRequest : IRequest<TResponse>
 {
     private readonly ILogger<LoggingBehavior<TRequest, TResponse>> _logger;
     private readonly TimeProvider _timeProvider;
