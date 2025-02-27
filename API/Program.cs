8a81cf9fa164c2da4ca1da9365b3bd7b4677b448
Enrico Roselino <roselino.dev@gmail.com>
2025-02-27 15:48:57 +0700

    add json setup

--- API/Program.cs
+++ API/Program.cs
@@ -3,7 +3,9 @@ using BuildingBlocks.API.Configurations;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddOpenApi();
+
 builder.Services.AddLoggerSetup();
+builder.Services.AddJsonSetup();
 builder.Services.AddIdempotentSetup();
 
 var app = builder.Build();
