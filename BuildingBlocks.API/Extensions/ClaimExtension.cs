4ab144d1eabcdeaae27a36b5010f2fe3fa1b65e0
Enrico Roselino <roselino.dev@gmail.com>
2025-03-03 13:45:34 +0700

    update to return null for ClaimExtension.cs

--- BuildingBlocks.API/Extensions/ClaimExtension.cs
+++ BuildingBlocks.API/Extensions/ClaimExtension.cs
@@ -7,21 +7,21 @@ namespace BuildingBlocks.API.Extensions;
 
 public static class ClaimExtensions
 {
-    public static Guid GetUserId(this ClaimsPrincipal claimsPrincipal)
+    public static Guid? GetUserId(this ClaimsPrincipal claimsPrincipal)
     {
         // somehow the 'sub' is translated into ClaimsTypes.NameIdentifier
         var sub = claimsPrincipal.FindFirstValue(ClaimTypes.NameIdentifier);
-        if (!Guid.TryParse(sub, out var userId) || Guid.Empty.Equals(userId)) throw new UnauthorizedException();
+        if (!Guid.TryParse(sub, out var userId) || Guid.Empty.Equals(userId)) return null;
         return userId;
     }
 
-    public static Guid GetTokenId(this ClaimsPrincipal claimsPrincipal)
+    public static Guid? GetTokenId(this ClaimsPrincipal claimsPrincipal)
     {
         var jti = claimsPrincipal.FindFirstValue(JwtRegisteredClaimNames.Jti);
-        if (!Guid.TryParse(jti, out var tokenId) || Guid.Empty.Equals(tokenId)) throw new UnauthorizedException();
+        if (!Guid.TryParse(jti, out var tokenId) || Guid.Empty.Equals(tokenId)) return null;
         return tokenId;
     }
-    
+
     public static bool IsImpersonating(this ClaimsPrincipal user)
     {
         return user.HasClaim(c => c.Type == CustomClaim.ImpersonatorId);
