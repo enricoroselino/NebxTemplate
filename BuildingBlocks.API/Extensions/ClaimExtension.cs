6ecc63aa7f5d15d055bdab80c02ed81c275da4db
Enrico Roselino <roselino.dev@gmail.com>
2025-03-03 14:11:10 +0700

    refactor ClaimExtension.cs

--- BuildingBlocks.API/Extensions/ClaimExtension.cs
+++ BuildingBlocks.API/Extensions/ClaimExtension.cs
@@ -7,17 +7,17 @@ namespace BuildingBlocks.API.Extensions;
 
 public static class ClaimExtensions
 {
-    public static Guid? GetUserId(this ClaimsPrincipal claimsPrincipal)
+    public static Guid? GetUserId(this ClaimsPrincipal user)
     {
         // somehow the 'sub' is translated into ClaimsTypes.NameIdentifier
-        var sub = claimsPrincipal.FindFirstValue(ClaimTypes.NameIdentifier);
+        var sub = user.FindFirstValue(ClaimTypes.NameIdentifier);
         if (!Guid.TryParse(sub, out var userId) || Guid.Empty.Equals(userId)) return null;
         return userId;
     }
 
-    public static Guid? GetTokenId(this ClaimsPrincipal claimsPrincipal)
+    public static Guid? GetTokenId(this ClaimsPrincipal user)
     {
-        var jti = claimsPrincipal.FindFirstValue(JwtRegisteredClaimNames.Jti);
+        var jti = user.FindFirstValue(JwtRegisteredClaimNames.Jti);
         if (!Guid.TryParse(jti, out var tokenId) || Guid.Empty.Equals(tokenId)) return null;
         return tokenId;
     }
@@ -25,5 +25,12 @@ public static class ClaimExtensions
     public static bool IsImpersonating(this ClaimsPrincipal user)
     {
         return user.HasClaim(c => c.Type == CustomClaim.ImpersonatorId);
+    }
+    
+    public static Guid? GetImpersonatorId(this ClaimsPrincipal user)
+    {
+        var sub = user.FindFirstValue(CustomClaim.ImpersonatorId);
+        if (!Guid.TryParse(sub, out var userId) || Guid.Empty.Equals(userId)) return null;
+        return userId;
     }
 }
