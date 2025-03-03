527014fc54b200496a923bf2ed2e28bc1d555052
Enrico Roselino <roselino.dev@gmail.com>
2025-03-03 11:05:50 +0700

    implemented register

--- Modules.Identity/Features/Register/RegisterCommandHandler.cs
+++ Modules.Identity/Features/Register/RegisterCommandHandler.cs
@@ -1,5 +1,9 @@
+using BuildingBlocks.API.Extensions;
 using BuildingBlocks.API.Models.CQRS;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Modules.Identity.Data;
+using Modules.Identity.Domain.Models;
 using Shared.Verdict;
 
 namespace Modules.Identity.Features.Register;
@@ -7,14 +11,28 @@ namespace Modules.Identity.Features.Register;
 public class RegisterCommandHandler : ICommandHandler<RegisterCommand, Verdict>
 {
     private readonly AppIdentityDbContext _dbContext;
+    private readonly UserManager<User> _userManager;
 
-    public RegisterCommandHandler(AppIdentityDbContext dbContext)
+    public RegisterCommandHandler(AppIdentityDbContext dbContext, UserManager<User> userManager)
     {
         _dbContext = dbContext;
+        _userManager = userManager;
     }
 
-    public Task<Verdict> Handle(RegisterCommand request, CancellationToken cancellationToken)
+    public async Task<Verdict> Handle(RegisterCommand request, CancellationToken cancellationToken)
     {
-        return Task.FromResult(Verdict.Success());
+        var newUser = User.Create(request.Username, request.Email, request.Fullname);
+
+        var existing = await _dbContext.Users
+            .AsNoTracking()
+            .Where(x =>
+                x.NormalizedUserName == request.Username.ToUpperInvariant() ||
+                x.NormalizedEmail == request.Email.ToUpperInvariant())
+            .SingleOrDefaultAsync(cancellationToken);
+        
+        if (existing is not null) return Verdict.Conflict("User already exists.");
+        
+        var result = await _userManager.CreateAsync(newUser, request.Password);
+        return !result.Succeeded ? Verdict.InternalError(result.GetErrors()) : Verdict.NoContent();
     }
 }
