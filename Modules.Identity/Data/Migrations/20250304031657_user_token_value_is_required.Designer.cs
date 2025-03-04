3e4fd589f8539d80db93f854426e7e20018ce929
Enrico Roselino <roselino.dev@gmail.com>
2025-03-04 10:23:18 +0700

    make user token value is not nullable

--- Modules.Identity/Data/Migrations/20250304031657_user_token_value_is_required.Designer.cs
+++ Modules.Identity/Data/Migrations/20250304031657_user_token_value_is_required.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Modules.Identity.Data;
 
@@ -11,9 +12,11 @@ using Modules.Identity.Data;
 namespace Modules.Identity.Data.Migrations
 {
     [DbContext(typeof(AppIdentityDbContext))]
-    partial class AppIdentityDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250304031657_user_token_value_is_required")]
+    partial class user_token_value_is_required
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- Modules.Identity/Domain/Models/UserToken.cs
+++ Modules.Identity/Domain/Models/UserToken.cs
@@ -1,10 +1,13 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Shared.Models.Interfaces;
+#pragma warning disable CS8765 // Nullability of type of parameter doesn't match overridden member (possibly because of nullability attributes).
 
 namespace Modules.Identity.Domain.Models;
 
 public class UserToken : IdentityUserToken<Guid>, ITimeAuditable
 {
+    public override string Value { get; set; } = string.Empty;
+    
     public DateTime CreatedOn { get; set; }
     public DateTime? ModifiedOn { get; set; }
 
