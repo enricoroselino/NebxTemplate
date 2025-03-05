0fc71dc1607721af8df795e76bcd38bbf06bdde9
Enrico Roselino <roselino.dev@gmail.com>
2025-03-05 14:25:24 +0700

    implemented delete roles

---
+++ Modules.Identity/Application/Features/AccessManagement/Roles/DeleteRoles/DeleteRolesCommandValidator.cs
@@ -0,0 +1,9 @@
+﻿namespace Modules.Identity.Application.Features.AccessManagement.Roles.DeleteRoles;
+
+public class DeleteRolesCommandValidator : AbstractValidator<DeleteRolesCommand>
+{
+    public DeleteRolesCommandValidator()
+    {
+        RuleFor(x => x.RoleId).NotEqual(Guid.Empty);
+    }
+}
---
+++ Modules.Identity/Application/Features/AccessManagement/Roles/DeleteRoles/DeleteRolesEndpoint.cs
@@ -0,0 +1,27 @@
+﻿using BuildingBlocks.API.Configurations.Endpoint;
+using Modules.Identity.Constants;
+
+namespace Modules.Identity.Application.Features.AccessManagement.Roles.DeleteRoles;
+
+public class DeleteRolesEndpoint : IEndpoint
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        var group = app.MapGroup(ApiMeta.AccessManagement.Route);
+
+        group.MapDelete("roles/{roleId:guid}", async (
+                ISender mediator,
+                HttpContext httpContext,
+                CancellationToken ct,
+                [FromRoute] Guid roleId) =>
+            {
+                var command = new DeleteRolesCommand(roleId);
+                var result = await mediator.Send(command, ct);
+                return result.ToResult(httpContext);
+            })
+            .WithName(nameof(DeleteRolesEndpoint))
+            .WithSummary("Deletes a role")
+            .WithTags(ApiMeta.AccessManagement.Tag)
+            .RequireAuthorization();
+    }
+}
